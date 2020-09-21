eb1916466a5bbbbe02d84ed3d0ab2721363f5122
Dina <diana.liubarets@hotmail.com>
2020-09-20 22:44:16 -0400

    deploying

--- server.js
+++ server.js
@@ -14,7 +14,10 @@ require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 });
 
 app.listen(PORT, function() {
